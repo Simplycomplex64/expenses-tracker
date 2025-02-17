d23f1b79dea3c980ce1e344f91af92e653662466
simplyComplex <91500514+Simplycomplex64@users.noreply.github.com>
2025-02-17 16:39:25 -0500

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import ExpensesTrackerHeader from "./components/ETheader folder/expenses-tracker-header";
-import FormSection from "./components/mainSection/FormSection";
-import GridSection from "./components/grid/gridSection";
+import { useState } from "react";
+import ExpenseTracker from "./expense-tracker/components/expense-tracker";
 
 const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "James", amount: 34, category: "Parent" },
+    { id: 2, description: "Jake", amount: 8, category: "Child" },
+    { id: 3, description: "Jane", amount: 2, category: "Child" },
+    { id: 4, description: "Josh", amount: 6, category: "Child" },
+  ]);
+
   return (
     <>
-      <div>
-        <ExpensesTrackerHeader />
-      </div>
-      <FormSection />
-      <GridSection />
+      <ExpenseTracker
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </>
   );
 };
