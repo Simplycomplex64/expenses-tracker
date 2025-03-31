9dc169dd2224ef6e0c158472b2d62109252fa7df
simplyComplex <91500514+Simplycomplex64@users.noreply.github.com>
2025-03-31 18:10:46 -0400

    lol

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,49 @@ import ExpenseFilter from "./expense-tracker/components/expense-filter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import categories from "./expense-tracker/components/categories";
 import ProductList from "./connect-to-back-end/ProductList";
+import axios, { AxiosError, CanceledError } from "axios";
+
+interface Users {
+  id: number;
+  name: string;
+}
 
 const App = () => {
-  const [category, setcategory] = useState("");
+  const [users, setUsers] = useState<Users[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const deleteUser = (user: Users) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  useEffect(() => {
+    const constroller = new AbortController();
+    setIsLoading(true);
+    axios
+      .get<Users[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: constroller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+    return () => constroller.abort();
+  }, []);
+  // const [category, setcategory] = useState("");
+
   // const [selectedFilter, setSelectedFilter] = useState("");
 
   // const [expenses, setExpenses] = useState([
@@ -23,18 +63,39 @@ const App = () => {
 
   return (
     <>
-      <div>
-        <select
-          className="form-select"
-          onChange={(event) => setcategory(event?.target.value)}
-        >
-          <option value=""></option>
-          <option value="Clothing">Clothing</option>
-          <option value="Household">Household</option>
-        </select>
-        <ProductList category={category} />
-      </div>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex align-items-center justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
+    // <>
+    //   <div>
+    //     <select
+    //       className="form-select"
+    //       onChange={(event) => setcategory(event?.target.value)}
+    //     >
+    //       <option value=""></option>
+    //       <option value="Clothing">Clothing</option>
+    //       <option value="Household">Household</option>
+    //     </select>
+    //     <ProductList category={category} />
+    //   </div>
+    // </>
+
     // <div>
     //   <div className="mb-5">
     //     <ExpenseForm
